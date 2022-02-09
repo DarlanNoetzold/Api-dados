7ab521af8bedfdc4994a7cd7855e90ff19da39d4
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-09 12:00:13 -0300

    Create Repositorios.java

---
+++ src/main/java/tech/noetzold/Apidarlan/contato/repository/Repositorios.java
@@ -0,0 +1,9 @@
+package tech.noetzold.Apidarlan.contato.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tech.noetzold.Apidarlan.contato.model.Repositorio;
+
+public interface Repositorios extends JpaRepository<Repositorio, Long> {
+
+}
