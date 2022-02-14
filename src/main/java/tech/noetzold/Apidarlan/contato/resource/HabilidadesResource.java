ed2f1abc6c765aec10cc8082265d66208e3bb2ed
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-14 09:06:07 -0300

    Create HabilidadesResource.java

---
+++ src/main/java/tech/noetzold/Apidarlan/contato/resource/HabilidadesResource.java
@@ -0,0 +1,67 @@
+package tech.noetzold.Apidarlan.contato.resource;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.Apidarlan.contato.model.Habilidade;
+import tech.noetzold.Apidarlan.contato.repository.Habilidades;
+
+import javax.validation.Valid;
+import java.util.List;
+
+public class HabilidadesResource {
+    @Autowired
+    private Habilidades habilidades;
+
+    @PostMapping
+    public Habilidade adicionar(@Valid @RequestBody Habilidade habilidade) {
+        return habilidades.save(habilidade);
+    }
+
+    @GetMapping
+    public List<Habilidade> listar() {
+        return habilidades.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Habilidade> buscar(@PathVariable Long id) {
+        Habilidade habilidade = habilidades.getOne(id);
+
+        if (habilidade == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(habilidade);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Habilidade> atualizar(@PathVariable Long id,
+                                             @Valid @RequestBody Habilidade habilidade) {
+        Habilidade existente = habilidades.getOne(id);
+
+        if (existente == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        BeanUtils.copyProperties(habilidade, existente, "id");
+
+        existente = habilidades.save(existente);
+
+        return ResponseEntity.ok(existente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        Habilidade habilidade = habilidades.getOne(id);
+
+        if (habilidade == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        habilidades.delete(habilidade);
+
+        return ResponseEntity.noContent().build();
+    }
+}
+
