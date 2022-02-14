de74b49f146f0fe916c88f4b8b75bd2ad509e02a
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-14 09:06:03 -0300

    Create Habilidades.java

---
+++ src/main/java/tech/noetzold/Apidarlan/contato/repository/Habilidades.java
@@ -0,0 +1,9 @@
+package tech.noetzold.Apidarlan.contato.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.Apidarlan.contato.model.Habilidade;
+
+public interface Habilidades extends JpaRepository<Habilidade, Long> {
+
+}
+
