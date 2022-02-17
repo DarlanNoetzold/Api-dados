d00ad7445389f7d6935bb7da8f983217f0cc3336
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-17 07:01:53 -0300

    Create Especialidades.java

---
+++ src/main/java/tech/noetzold/Apidarlan/contato/repository/Especialidades.java
@@ -0,0 +1,8 @@
+package tech.noetzold.Apidarlan.contato.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.Apidarlan.contato.model.Especialidade;
+
+public interface Especialidades extends JpaRepository<Especialidade, Long> {
+
+}
