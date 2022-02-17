41819a60abf85cbb26b9596b19199d14316832c5
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-17 07:01:56 -0300

    Create EspecialidadesResource.java

---
+++ src/main/java/tech/noetzold/Apidarlan/contato/resource/EspecialidadesResource.java
@@ -0,0 +1,66 @@
+package tech.noetzold.Apidarlan.contato.resource;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.Apidarlan.contato.model.Especialidade;
+import tech.noetzold.Apidarlan.contato.repository.Especialidades;
+
+import javax.validation.Valid;
+import java.util.List;
+
+public class EspecialidadesResource {
+    @Autowired
+    private Especialidades especialidades;
+
+    @PostMapping
+    public Especialidade adicionar(@Valid @RequestBody Especialidade especialidade) {
+        return especialidades.save(especialidade);
+    }
+
+    @GetMapping
+    public List<Especialidade> listar() {
+        return especialidades.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Especialidade> buscar(@PathVariable Long id) {
+        Especialidade especialidade = especialidades.getOne(id);
+
+        if (especialidade == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(especialidade);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Especialidade> atualizar(@PathVariable Long id,
+                                                @Valid @RequestBody Especialidade especialidade) {
+        Especialidade existente = especialidades.getOne(id);
+
+        if (existente == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        BeanUtils.copyProperties(especialidade, existente, "id");
+
+        existente = especialidades.save(existente);
+
+        return ResponseEntity.ok(existente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        Especialidade especialidade = especialidades.getOne(id);
+
+        if (especialidade == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        especialidades.delete(especialidade);
+
+        return ResponseEntity.noContent().build();
+    }
+}
