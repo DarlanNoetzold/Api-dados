8ef7eb62a57fe132c3527e43fe602b316bc335e9
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-21 06:39:34 -0300

    Update Certificados.java

--- src/main/java/tech/noetzold/Apidarlan/contato/repository/Certificados.java
+++ src/main/java/tech/noetzold/Apidarlan/contato/repository/Certificados.java
@@ -1,4 +1,8 @@
 package tech.noetzold.Apidarlan.contato.repository;
 
-public class Certificados {
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.Apidarlan.contato.model.Certificado;
+
+public interface Certificados extends JpaRepository<Certificado, Long> {
+
 }
