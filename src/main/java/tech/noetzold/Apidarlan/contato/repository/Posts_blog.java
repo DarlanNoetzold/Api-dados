05254f70eedb8236e557a14628743d0fcf641bb4
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-02-26 06:34:48 -0300

    Update Posts_blog.java

--- src/main/java/tech/noetzold/Apidarlan/contato/repository/Posts_blog.java
+++ src/main/java/tech/noetzold/Apidarlan/contato/repository/Posts_blog.java
@@ -1,4 +1,7 @@
 package tech.noetzold.Apidarlan.contato.repository;
 
-public class Posts_blog {
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.Apidarlan.contato.model.Post_blog;
+
+public interface Posts_blog extends JpaRepository<Post_blog, Long> {
 }
