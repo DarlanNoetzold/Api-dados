d10693d2445d27e875f74917df99d42915b52e89
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-03-09 09:41:07 -0300

    Update Contatos.java

--- src/main/java/tech/noetzold/Apidarlan/contato/repository/Contatos.java
+++ src/main/java/tech/noetzold/Apidarlan/contato/repository/Contatos.java
@@ -1,7 +1,6 @@
 package tech.noetzold.Apidarlan.contato.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import tech.noetzold.Apidarlan.contato.model.Contato;
 
 public interface Contatos extends JpaRepository<Contato, Long> {
