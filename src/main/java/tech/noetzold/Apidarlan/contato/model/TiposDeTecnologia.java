02eb47a75013d4d68b189631c609e90e7237f96a
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-03-27 06:53:46 -0300

    Update TiposDeTecnologia.java

--- src/main/java/tech/noetzold/Apidarlan/contato/model/TiposDeTecnologia.java
+++ src/main/java/tech/noetzold/Apidarlan/contato/model/TiposDeTecnologia.java
@@ -1,5 +1,5 @@
 package tech.noetzold.Apidarlan.contato.model;
 
 public enum TiposDeTecnologia {
-    LINGUAGEM, BANCO, MENSAGEIRIA, SEARCHENGINE, VERSIONADOR
+    LINGUAGEM, BANCO, MENSAGEIRIA, SEARCHENGINE, VERSIONADOR, MICROSSERVIÇOS
 }
